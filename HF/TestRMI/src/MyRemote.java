21a2ce41cbd395793f54762ca8ed9ace3c4bac0c
DIMA <batsurin@mail.ru>
2022-07-07 18:45:06 +0300

    add TestRMI2

--- HF/TestRMI/src/MyRemote.java
+++ HF/TestRMI/src/MyRemote.java
@@ -1,5 +1,6 @@
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface MyRemote extends Remote {
-    String sayHello() throws RuntimeException;
+    String sayHello() throws RemoteException;
 }
---
+++ HF/TestRMI2/src/RemoteCalculationServer.java
@@ -0,0 +1,9 @@
+import java.rmi.RemoteException;
+
+public class RemoteCalculationServer implements Calculator {
+
+    @Override
+    public int multiply(int x, int y) throws RemoteException {
+        return x * y;
+    }
+}
